b139118897f49fc783b432ba5e457bd6abf95192
Ayoub El hioui <ael-hiou@e1r2p6.1337.ma>
2023-05-30 20:41:25 +0100

    getting the authorization code from tha 42 Intra Api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,8 +10,6 @@ export class AuthController{
         console.log({
             req
         });
-        // res.setHeader('Location', 'https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a03da0e6296d63a6c503040864e2f87ed71295125a6d30ac9b58b101c977867e&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code');
-        // res.status(302).send();
     }
     @Get('auth')
     signIn(@Res() res: Response ) {
