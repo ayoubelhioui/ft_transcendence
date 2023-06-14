b7338f656b7cf3c33c41dc0359c804f61e84728c
Mouad El ammari <mel-amma@e1r1p9.1337.ma>
2023-06-14 15:05:11 +0100

    added user guard

---
+++ src/components/user/guards/user-exists.guard.ts
@@ -0,0 +1,24 @@
+import { BadRequestException, CanActivate, ExecutionContext, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { IUserRepository } from 'src/components/repositories/repositories_interfaces';
+import { User } from 'src/database/entities';
+
+@Injectable()
+export class UserExistGuard implements CanActivate {
+  constructor(@Inject('UserRepository') private readonly userRepository: IUserRepository,
+) {};
+
+  async canActivate(
+    context: ExecutionContext,
+  ):  Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const targetUserId : number = parseInt(request.params.targetUserId);
+    if (isNaN(targetUserId))
+            throw new BadRequestException("the Targeted UserId Must be a Number");
+    const user = await this.userRepository.findOneById(targetUserId);
+    if(!user)
+      throw new NotFoundException("Targeted user doesn't exist");
+    request.targetedUser = user;
+    return true;
+  }
+}
