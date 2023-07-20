9a75f699d20db0fc1852e219c4d733d0eb3b3d46
Mouad El ammari <mel-amma@e1r4p8.1337.ma>
2023-07-20 11:39:02 +0100

    deleted old auth for fetching the new one from merge

--- backend/src/components/auth/auth.constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-    secret: 'this is just a secret test',
-}
--- backend/src/components/auth/strategy/intra.strategy.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-
-@Injectable()
-export class Test
-{
-
-}
